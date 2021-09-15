f53936b0798ba7f7745cd0ae301c1f71e4aeec4a
ahmedmeshref <a.meshref@alustudent.com>
2021-09-15 23:30:39 +0200

    Build models

---
+++ Otthonbazar/Data/ApplicationDbContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Otthonbazar.Data.EntityTypeConfigurations;
+
+
+namespace Otthonbazar.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<City> City { get; set; }
+        public DbSet<Advertisement> Advertisement { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Add data 
+            builder.ApplyConfiguration(new CitySeedConfig());
+            builder.ApplyConfiguration(new AdvertisementSeedConfig());
+        }
+    }
+
+    
+
+    public enum AdvertisementType
+    {
+        Flat,
+        House,
+        HolidayHouse,
+        BuildingPlot
+    }
+
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        [StringLength(4, MinimumLength = 4)]
+        public int CityId { get; set; }
+        public AdvertisementType AdvertisementType { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required]
+        public int Room { get; set; }
+        [Required]
+        public int HalfRoom { get; set; }
+        [Required]
+        public int BuildDate { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        [Required, StringLength(4, MinimumLength = 4)]
+        public string Zip { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        [ForeignKey("CityId")]
+        public ICollection<Advertisement> Advertisements { get; set; }
+    }
+
+}
---
+++ Otthonbazar/Data/Migrations/20210915193604_AdvertisementsAndCities.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Otthonbazar.Data.Migrations
+{
+    public partial class AdvertisementsAndCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Zip = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityId = table.Column<int>(type: "int", maxLength: 4, nullable: false),
+                    AdvertisementType = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    Room = table.Column<int>(type: "int", nullable: false),
+                    HalfRoom = table.Column<int>(type: "int", nullable: false),
+                    BuildDate = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Advertisement_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisement_CityId",
+                table: "Advertisement",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Advertisement");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
