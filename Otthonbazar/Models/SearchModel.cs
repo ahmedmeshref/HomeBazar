3e1f82512946978289ea6c9bf56cc108f80d90fc
ahmedmeshref <a.meshref@alustudent.com>
2021-09-16 11:55:55 +0200

    Implement search

---
+++ Otthonbazar/Models/SearchModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Otthonbazar.Models
+{
+    public class SearchModel
+    {
+        public int CityId { get; set; }
+        [Display(Name = "City Name")]
+        public string CityName { get; set; }
+        [Display(Name = "Max Price")]
+        public Nullable<decimal> PriceMax { get; set; }
+        [Display(Name = "Min Price")]
+        public Nullable<decimal> PriceMin { get; set; }
+        [Display(Name = "Max Rooms")]
+        public Nullable<int> RoomMax { get; set; }
+        [Display(Name = "Min Rooms")]
+        public Nullable<int> RoomMin { get; set; }
+        [Display(Name = "Max Size")]
+        public Nullable<int> SizeMax { get; set; }
+        [Display(Name = "Min Size")]
+        public Nullable<int> SizeMin { get; set; }
+    }
+}
--- Otthonbazar/Pages/Advertisement/Index.cshtml.cs
+++ Otthonbazar/Pages/Advertisement/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Otthonbazar.Data;
+using Otthonbazar.Models;
 
 namespace Otthonbazar.Pages.Advertisement
 {
@@ -20,9 +21,34 @@ namespace Otthonbazar.Pages.Advertisement
 
         public IList<Otthonbazar.Data.Advertisement> Advertisement { get;set; }
 
+        /*
         public async Task OnGetAsync()
         {
             Advertisement = await _context.Advertisement.ToListAsync();
+        }*/
+
+        public async Task OnGetAsync()
+        {
+
+            IQueryable<Otthonbazar.Data.Advertisement> advertisements = _context.Advertisement;
+
+            if (Search.PriceMin != null)
+                advertisements = advertisements.Where(a => a.Price >= Search.PriceMin.Value);
+            if (Search.PriceMax != null)
+                advertisements = advertisements.Where(a => a.Price <= Search.PriceMax.Value);
+            if (Search.RoomMin != null)
+                advertisements = advertisements.Where(a => a.Room >= Search.RoomMin.Value);
+            if (Search.RoomMax != null)
+                advertisements = advertisements.Where(a => a.Room <= Search.RoomMax.Value);
+            if (Search.SizeMax != null)
+                advertisements = advertisements.Where(a => a.Price <= Search.SizeMax.Value);
+            if (Search.SizeMin != null)
+                advertisements = advertisements.Where(a => a.Price >= Search.SizeMin.Value);
+            Advertisement = await advertisements.ToListAsync();
         }
+
+
+        [BindProperty(SupportsGet = true)]
+        public SearchModel Search { get; set; }
     }
 }
